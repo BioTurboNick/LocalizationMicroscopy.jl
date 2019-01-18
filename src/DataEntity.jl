c0301935fd4973a6c1e2e992b9ba23f2df82e994
Nicholas Bauer <nicholasbauer@outlook.com>
2019-01-18 05:27:32 -0500

    Back to mapreduce for extractcoordinates, made type-stable returning 2d array

--- src/DataEntity.jl
+++ src/DataEntity.jl
@@ -7,6 +7,9 @@ abstract type DataEntity
 end
 
 extractcoordinates(entity::T) where {T <: DataEntity} = [entity.x; entity.y; entity.z]
-extractcoordinates(entities::Vector{<:DataEntity}) = hcat(extractcoordinates.(entities))
+extractcoordinates(entities::Vector{<:DataEntity}) =
+    length(entities) > 1 ? mapreduce(extractcoordinates, hcat, entities) : make2d(entities)
+
+make2d(array1d::AbstractVector) = reshape(array1d, length(array1d), 1)
 
 export DataEntity, extractcoordinates

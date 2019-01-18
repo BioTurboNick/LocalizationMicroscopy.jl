ae8d94ed353557e8d72d326255cbfedf3b75e5f5
Nicholas Bauer <nicholasbauer@outlook.com>
2019-01-18 06:01:12 -0500

    Fixed return type

--- src/DataEntity.jl
+++ src/DataEntity.jl
@@ -8,8 +8,7 @@ end
 
 extractcoordinates(entity::T) where {T <: DataEntity} = [entity.x; entity.y; entity.z]
 extractcoordinates(entities::Vector{<:DataEntity}) =
-    length(entities) > 1 ? mapreduce(extractcoordinates, hcat, entities) : make2d(entities)
-
+    mapreduce(extractcoordinates, hcat, entities) |> (length(entities) > 1 ? identity : make2d)
 make2d(array1d::AbstractVector) = reshape(array1d, length(array1d), 1)
 
 export DataEntity, extractcoordinates
